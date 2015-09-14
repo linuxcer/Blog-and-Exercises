fb7a108fc0fafc1bcdf2accd06141e2d035e5018
Chengfei Zhang <chengfeizh@gmail.com>
2015-09-14 14:42:36 +0800

    add debug char_point

---
+++ tmp/char_point.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+ *  --------->    FILE: char_point.cpp
+ *  --------->    AUTHOR: Aaron
+ *  --------->    MAIL: chengfeizh@gmail.com
+ *  --------->    DATE: 09/14/2015    TIME:14:12:30
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    char *str;
+    cout << (char *)str << endl;
+    printf("%p\n", str);
+    //str[0] = 'a';
+    //str[1] = 'b';
+    return 0;
+}
