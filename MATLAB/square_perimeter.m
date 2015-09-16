cd11770ba271b69d0486d37b143134976b8e4b56
Chengfei Zhang <chengfeizh@gmail.com>
2015-09-16 14:23:51 +0800

    add Matlab

---
+++ MATLAB/square_perimeter.m
@@ -0,0 +1,5 @@
+function [s, p] = square_perimeter(r)
+%CIRCLE calcuate the area and perimeter of a circle of radii r
+%r 半径
+s = pi * r * r;
+p = 2 * pi * r;
---
+++ tmp/shift.c
@@ -0,0 +1,25 @@
+/*************************************************************************
+ *  --------->    FILE: shift.c
+ *  --------->    AUTHOR: Aaron
+ *  --------->    MAIL: chengfeizh@gmail.com
+ *  --------->    DATE: 09/16/2015    TIME:08:40:25
+ ************************************************************************/
+
+#include <stdio.h>
+int main() {
+    char str[10] = "zhang";
+    int i;
+    printf("%p\n", str);
+    printf("%p\n", (long)str & 0xfffffffffffff000);
+    int start = 0;
+    for (i = 0; i < 4096; i++) {
+        if (*(char *)(((unsigned long)str & 0xfffffffffffff000) + i) == 'z') {
+            start = 1;
+            printf("%d %c\n", i, * (char *)(((unsigned long)str & 0xfffffffffffff000) + i));
+        }
+        if (start == 1) {
+            printf("%c", * (char *)(((unsigned long)str & 0xfffffffffffff000) + i));
+        }
+    }
+    return 0;
+}
